6d3e919c40da5bdb9c8c6d2245acf1b3f7bcef4a
Douglas Obara <douglasobara97@gmail.com>
2022-06-12 20:56:04 +0300

    commit tree ff895466771e3ea5a8e87c11d700f2d5c3929d7e

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
+//  returns an array with title case tutorial names:
+
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((line) => {
+    const tokens = line.split(" ");
+    const tokenWeCapitalize = tokens.map(
+      (indexToken) => indexToken.charAt(0).toUpperCase() + indexToken.slice(1)
+    );
+    const result = tokenWeCapitalize.join(" ");
+    return result;
+  });
+};
